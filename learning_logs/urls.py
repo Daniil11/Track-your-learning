d795fbbae7b236fc6502c2914617a50039f5c6bc
thekeisi <htht@yahoo.com>
2019-02-15 22:21:33 +0400

    Создал функцию редактирования

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
 	path('new_topic',views.new_topic,name='new_topic'),
 	#new
 	path('new_entry/(<topic_id>)', views.new_entry, name='new_entry'),
+	path('edit_entry/(<entry_id>)',views.edit_entry,name='edit_entry'),
 ]
