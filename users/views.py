09987c34b2af57a892997d5bee3b086a351c0a84
thekeisi <htht@yahoo.com>
2019-02-17 18:17:58 +0400

    Made a log out sys

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import logout
+def logout_view(request):
+	"""Завершает сеанс работы с приложением."""
+	logout(request)
+	return HttpResponseRedirect(reverse('learning_logs:index'))
