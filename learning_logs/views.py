014424bbeb34792671e302c4dae31a7f6e4b83c0
thekeisi <htht@yahoo.com>
2019-02-17 19:31:39 +0400

    Deploy1

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,22 +1,29 @@
 from django.shortcuts import render
 from .models import Topic as TopicModel
 from .models import Entry as EntryModel
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from .forms import TopicForm as TopicFormModel
 from .forms import EntryForm as EntryFormModel
+from django.contrib.auth.decorators import login_required
+
 def index(request):
 	return render(request, 'learning_logs/index.html')
+@login_required
 def topics(request):
 	#Внимание эта хуета работает только если топик импортировать как топикМодель!!!
-	topics = TopicModel.objects.order_by('date_added')
+	topics = TopicModel.objects.filter(owner=request.user).order_by('date_added')
 	context = {'topics': topics}
 	return render(request, 'learning_logs/topics.html',context)
+@login_required
 def topic(request,topic_id):
 	topic = TopicModel.objects.get(id=topic_id)
+	if topic.owner != request.user:
+		raise Http404
 	entries = topic.entry_set.order_by('-date_added')
 	context = {'topic':topic,'entries':entries}
 	return render(request, 'learning_logs/topic.html',context)
+@login_required
 def new_topic(request):
 
 	if request.method != 'POST':
@@ -24,10 +31,14 @@ def new_topic(request):
 	else:
 		form = TopicFormModel(request.POST)
 		if form.is_valid():
-			form.save()
+			new_topic = form.save(commit=False)
+			new_topic.owner = request.user
+			new_topic.save()
+			#form.save()
 			return HttpResponseRedirect(reverse('learning_logs:topics'))
 	context = {'form':form}
 	return render(request,'learning_logs/new_topic.html', context)
+@login_required
 def new_entry(request,topic_id):
 	topic = TopicModel.objects.get(id=topic_id)
 	if request.method != 'POST':
@@ -44,10 +55,12 @@ def new_entry(request,topic_id):
 
 	context = {'topic': topic, 'form': form}
 	return render(request, 'learning_logs/new_entry.html', context)
+@login_required
 def edit_entry(request,entry_id):
 	entry = EntryModel.objects.get(id=entry_id)
 	topic = entry.topic
-	
+	if topic.owner != request.user:
+		raise Http404
 	if request.method != 'POST':
 		form= EntryFormModel(instance=entry)
 	else:
